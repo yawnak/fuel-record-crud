6d88a683f06e1a5df328269ac5bcb32c87cf3417
Shliakhov Danylo <shliakhov.danylo@gmail.com>
2023-10-18 21:20:55 +0300

    stuff

--- internal/adapters/logadapt/logadapt.go
+++ internal/adapters/logadapt/logadapt.go
@@ -1,7 +1,18 @@
 package logadapt
 
-import "github.com/yawnak/fuel-record-crud/pkg/logger"
+import (
+	"github.com/rs/zerolog"
+	"github.com/yawnak/fuel-record-crud/pkg/logger"
+)
 
-func NewLoggerAdapter() logger.Logger {
+func NewStagingLoggerAdapter() logger.Logger {
 	return logger.NewLogger()
+}
+
+func NewProductionLoggerAdapter() logger.Logger {
+	return logger.NewLogger()
+}
+
+func NewDevelopmentLoggerAdapter() logger.Logger {
+	return logger.NewLogger(logger.WithConsoleWriter(), logger.WithLevel(zerolog.DebugLevel))
 }
